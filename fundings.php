55d27cbd6663bd371e3209977ee51ff28a1fd914
Chinmay Singh <chinmay22897@gmail.com>
2020-02-28 16:20:01 +0530

    Add files via upload

---
+++ fundings.php
@@ -0,0 +1,153 @@
+<?php
+require "db.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <title>Funding Rounds</title>
+   <style>
+      body {
+         font-family: Arial, Helvetica, sans-serif;
+      }
+
+      /* The Modal (background) */
+      .modal {
+         display: none;
+         /* Hidden by default */
+         position: fixed;
+         /* Stay in place */
+         z-index: 1;
+         /* Sit on top */
+         padding-top: 100px;
+         /* Location of the box */
+         left: 0;
+         top: 0;
+         width: 100%;
+         /* Full width */
+         height: 100%;
+         /* Full height */
+         overflow: auto;
+         /* Enable scroll if needed */
+         background-color: rgb(0, 0, 0);
+         /* Fallback color */
+         background-color: rgba(0, 0, 0, 0.4);
+         /* Black w/ opacity */
+      }
+
+      /* Modal Content */
+      .modal-content {
+         background-color: #fefefe;
+         margin: auto;
+         padding: 20px;
+         border: 1px solid #888;
+         width: 80%;
+      }
+
+      /* The Close Button */
+      .close {
+         color: #aaaaaa;
+         float: right;
+         font-size: 28px;
+         font-weight: bold;
+      }
+
+      .close:hover,
+      .close:focus {
+         color: #000;
+         text-decoration: none;
+         cursor: pointer;
+      }
+   </style>
+</head>
+
+<body>
+   <?php
+   $sql = "Select q1.*,q2.* from
+(Select nm.name,funding_rounds.* from funding_rounds
+left join
+(select id,name from objects) as nm
+on nm.id = funding_rounds.object_id) as q1
+left join
+(Select t.funding_round_id,STRING_AGG(t.iname,',') as investors
+from (Select nm.name as iname,investments.* from 
+(select id,name from objects) as nm,investments
+where nm.id = investments.investor_object_id) as t
+group by t.funding_round_id) as q2
+on q1.funding_round_id = q2.funding_round_id
+order by q1.funded_at DESC NULLS last
+;";
+
+   $result = pg_query($db, $sql);
+   if (!$result) {
+      echo pg_last_error($db);
+      exit;
+   }
+   ?>
+   <table class=\"table\">
+      <tr>
+         <th>Organization Name</th>
+         <th>Transaction Name</th>
+         <th>Money Raised</th>
+		 <th>Investors </th>
+      </tr>
+      <?php
+      // output data of each row
+      while ($row = pg_fetch_row($result)) { ?>
+
+         <tr>
+            <td><a href="company.php?id=<?php echo $row[3]; ?>"><?php echo $row[0]; ?></a></td>
+            <td><a href="<?php echo $row[19];?>"><?php echo $row[5]; ?></a></td>
+            <td>
+               <button id="myBtn"><?php echo $row[9] . " ";echo $row[8]; ?></button>
+
+               <!-- The Modal -->
+               <div id="myModal" class="modal">
+
+                  <!-- Modal content -->
+                  <div class="modal-content">
+                     <span class="close">&times;</span>
+                     <p><?php echo $row[20]; ?></p>
+                  </div>
+
+               </div>
+
+            </td>
+			<td><?php echo $row[25]; ?></a></td>
+         </tr>
+      <?php
+      } ?>
+   </table>
+   <?php
+   pg_close($db);
+   ?>
+   <script>
+      // Get the modal
+      var modal = document.getElementById("myModal");
+
+      // Get the button that opens the modal
+      var btn = document.getElementById("myBtn");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks the button, open the modal 
+      btn.onclick = function() {
+         modal.style.display = "block";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+         modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+         if (event.target == modal) {
+            modal.style.display = "none";
+         }
+      }
+   </script>
+</body>
+
+</html>
---
+++ main_page.php
@@ -0,0 +1,125 @@
+<?php
+
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
+require "db.php";
+$sql = "Select q1.*,q2.* from
+(Select nm.name,funding_rounds.* from funding_rounds
+left join
+(select id,name from objects) as nm
+on nm.id = funding_rounds.object_id) as q1
+left join
+(Select t.funding_round_id,STRING_AGG(t.iname,',') as investors
+from (Select nm.name as iname,investments.* from 
+(select id,name from objects) as nm,investments
+where nm.id = investments.investor_object_id) as t
+group by t.funding_round_id) as q2
+on q1.funding_round_id = q2.funding_round_id
+order by q1.funded_at DESC NULLS last
+LIMIT 10;"
+
+$result = pg_query($db, $sql);
+if (!$result) {
+    echo pg_last_error($db);
+    exit;
+	
+$sql1 = "select q1.acquiring_object_id,q2.acquired_object_id,q1.By,q2.OF,q1.price_amount,q1.price_currency_code,q2.acquired_at,q2.source_description from
+      (select acquisitions.id,acquiring_object_id,normalized_name as By,price_amount,price_currency_code
+       from acquisitions
+      left join objects on objects.id=acquisitions.acquiring_object_id
+      order by id) as q1 
+      left join
+      (select acquisitions.id,acquired_object_id,normalized_name as OF,acquired_at,source_description
+       from acquisitions
+      left join objects on objects.id=acquisitions.acquired_object_id
+      order by id) as q2
+      on q1.id=q2.id
+      order by acquired_at desc,price_amount desc
+	  LIMIT 10;";
+
+   $result1 = pg_query($db, $sql1);
+   if (!$result1) {
+      echo pg_last_error($db);
+      exit;
+   }
+	
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Search Engine</title>
+</head>
+
+<body>
+	<form id="searchForm" action="search_res.php" method="get" autocomplete="off">
+        <h1>Search!</h1>
+        <div class="row">
+            <div class="col-sm-6">
+                <h5><b>Search For Anything</b></h5>
+                <input type="text" placeholder="Search" name="fname" required><br>
+            </div>
+        </div>
+    </form>
+
+	<table class=\"table\">
+      <tr>
+         <th>Acquiree Name</th>
+         <th>Acquirer Name</th>
+         <th>Acquired At</th>
+         <th>Price</th>
+      </tr>
+      <?php
+      // output data of each row
+      while ($row1 = pg_fetch_row($result1)) { ?>
+
+         <tr>
+            <td><a href="company.php?id=<?php echo $row1[0]; ?>"><?php echo $row1[2]; ?></a></td>
+            <td><a href="company.php?id=<?php echo $row1[1]; ?>"><?php echo $row1[3]; ?></a></td>
+            <td> <?php echo $row1[6]; ?> </td>
+     
+			<td><?php echo $row1[7]; ?></a></td>
+            <td> <?php echo $row1[5] . " "; echo $row1[4]; ?> </td>
+         </tr>
+      <?php
+      } ?>
+	  <tr> <a href="acquisitions.php">Show all</a><\tr>
+   </table>
+
+
+    <table class=\"table\">
+        <tr>
+            <th>Organization Name</th>
+            <th>Transaction Name</th>
+            <th>Money Raised</th>
+			<th>Investors </th>
+        </tr>
+        <?php
+        // output data of each row
+        while ($row = pg_fetch_row($result)) { ?>
+
+            <tr>
+                
+                <td><a href="company.php?id=<?php echo $row[3]; ?>"><?php echo $row[0]; ?></a></td>
+                <td><?php echo $row[5]; ?></td>
+                <td><?php echo $row[7]; ?></a></td>
+                <td><?php echo $row[25]; ?> </td>
+            </tr>
+        <?php
+        } ?>
+		<tr> <a href="fundings.php">Show all</a><\tr>
+    </table>
+    <?php
+    pg_close($db);
+    ?>
+
+</body>
+
+</html>
